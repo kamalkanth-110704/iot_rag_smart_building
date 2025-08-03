e4400dbcf4c6d8e94a45f0fb4fb56df7c94f5299
kamalkanth-110704 <kamalkanthsilla@gmail.com>
2025-08-03 20:05:34 +0530

    Fix start.sh for Render deployment

--- start.sh
+++ start.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 pip install --upgrade pip
-streamlit run app.py --server.port $PORT --server.address 0.0.0.0
+pip install -r requirements.txt
+PORT=${PORT:-10000}
+streamlit run app.py --server.port=$PORT --server.address=0.0.0.0
